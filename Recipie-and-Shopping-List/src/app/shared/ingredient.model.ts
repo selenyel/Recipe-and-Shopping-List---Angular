9a139d9f0db1185a8d188bb53deb5ba3298c031c
selenyel <selenyelyazilim@gmail.com>
2023-06-05 11:53:02 +0300

    Lesson 60 - Adding Shopping List sections
    Created shopping List mock values and style them.
    Added Shopping 🛒  List editing buttons.
    Created a new model called ingredient which has a name property and an amount property.
    Created a shared list that I put my new ingredient model

---
+++ Recipie-and-Shopping-List/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient{
+    // public name:string;
+    // public amount: number;
+
+    constructor(public name:string, public amount:number){ // same as up there , automatically assigns the name and amount
+    }
+}
